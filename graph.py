b00face501d75c962e843738fb5169fe13eb1e48
k-sobczyk <ksobczyk.pv@gmail.com>
2023-11-08 21:50:46 +0100

    add dfs, bfs and GraphIterator

--- graph.py
+++ graph.py
@@ -63,24 +63,79 @@ class Graph:
         for vertex in self.graph:
             print(f"{vertex}: {self.graph[vertex]}")
 
+    def dfs(self, start_vertex: Any):
+        if not self.check_if_valid_key_type(start_vertex):
+            raise ValueError(f"Podana wartość nie może byc kluczem grafu.")
+        elif start_vertex not in self.graph:
+            raise ValueError(f"Podany wierzchołek '{start_vertex}' nie istnieje w grafie.")
+
+        def dfs_recursive(vertex, visited):
+            visited.add(vertex)
+            result.append(vertex)
+            for neighbor in self.graph[vertex]:
+                if neighbor not in visited:
+                    dfs_recursive(neighbor, visited)
+
+        visited = set()
+        result = []
+        dfs_recursive(start_vertex, visited)
+        return result
+
+    def bfs(self, start_vertex: Any):
+        if not self.check_if_valid_key_type(start_vertex):
+            raise ValueError(f"Podana wartość nie może byc kluczem grafu.")
+        elif start_vertex not in self.graph:
+            raise ValueError(f"Podany wierzchołek '{start_vertex}' nie należy do grafu.")
+
+        visited = set()
+        result = []
+        queue = [start_vertex]
+
+        while queue:
+            current_vertex = queue.pop(0)
+            if current_vertex not in visited:
+                visited.add(current_vertex)
+                result.append(current_vertex)
+                queue.extend(neighbor for neighbor in self.graph[current_vertex] if neighbor not in visited)
+
+        return result
+
+    def __iter__(self):
+        return GraphIterator(self.graph)
+
 
 class GraphIterator:
-    pass
+    def __init__(self, graph):
+        self.graph = graph
+        self.vertices = list(graph.keys())
+        self.current_index = 0
+
+    def __next__(self):
+        if self.current_index < len(self.vertices):
+            current_vertex = self.vertices[self.current_index]
+            self.current_index += 1
+            return current_vertex
+        else:
+            raise StopIteration
 
 
 def main():
+    # Przygotowanie grafu
     graph = Graph()
 
     graph.add_vertex('A')
     graph.add_vertex('B')
     graph.add_vertex('C')
     graph.add_vertex('D')
+    graph.add_vertex('E')
 
     graph.add_edge('A', 'B')
     graph.add_edge('A', 'C')
     graph.add_edge('B', 'C')
     graph.add_edge('A', 'D')
+    graph.add_edge('D', 'E')
 
+    # test metod grafu
     print("Graf po dodaniu wierzchołków i krawędzi:")
     graph.print_graph()
 
@@ -89,6 +144,15 @@ def main():
     graph.remove_edge('A', 'C')
     print("Graf po usunięciu wierzchołka 'B' oraz krawędzi między 'A' a 'C':")
     graph.print_graph()
+
+    # test końcówki zadania
+    print("DFS:")
+    for vertex in graph.dfs("A"):
+        print(vertex)
+
+    print("\nBFS:")
+    for vertex in graph.bfs("A"):
+        print(vertex)
 
 
 if __name__ == "__main__":
