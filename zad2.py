fd8600f237dfd40350e0b592f2f248c4904e915d
k-sobczyk <ksobczyk.pv@gmail.com>
2023-11-16 22:27:46 +0100

    changing functions name and fixing bug in count_ngrams

--- zad2.py
+++ zad2.py
@@ -1,24 +1,25 @@
 from collections import Counter
 
 
-def display_top_engrams(file_path, n):
+def count_ngrams(file_path, n):
     ngram_counter = Counter()
 
     with open(file_path, 'r', encoding='utf-8') as in_file:
         for line in in_file:
             words = line.lower().split()
-            ngrams = [words[i:i + n] for i in range(len(words) - n + 1)]
+            ngrams = (tuple(words[i:i + n]) for i in range(len(words) - n + 1))
             ngram_counter.update(ngrams)
 
         return ngram_counter
 
 
-def count_ngrams():
+def display_top_ngrams():
     pass
 
 
 def main():
-    pass
+    ngram_results = count_ngrams('szymborska.txt', 3)
+    print(ngram_results)
 
 
 if __name__ == '__main__':
