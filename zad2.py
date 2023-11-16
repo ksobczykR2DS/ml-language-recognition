79c0bfcf5b7fe1d855a07bb0dbfac1350a286345
k-sobczyk <ksobczyk.pv@gmail.com>
2023-11-16 22:08:52 +0100

    adding display_top_ngrams function

--- zad2.py
+++ zad2.py
@@ -1,5 +1,16 @@
-def display_top_engrams():
-    pass
+from collections import Counter
+
+
+def display_top_engrams(file_path, n):
+    ngram_counter = Counter()
+
+    with open(file_path, 'r', encoding='utf-8') as in_file:
+        for line in in_file:
+            words = line.lower().split()
+            ngrams = [words[i:i + n] for i in range(len(words) - n + 1)]
+            ngram_counter.update(ngrams)
+
+        return ngram_counter
 
 
 def count_ngrams():
